7606dd183140be8a31ff3afbe69c454b29732a7d
emilyrn18 <emilyrn18@vt.edu>
2022-04-23 01:25:15 -0400

    added routes

--- index.js
+++ index.js
@@ -4,10 +4,33 @@ const port = 8000;                  //port number where your server will be list
 const path = require('path');
 
 // app.set('html', __dirname + 'html')
+app.use(express.static(__dirname + '/public'));
+
 
 app.get('/', (req, res) => {        //get requests to the root ("/") will route here
-    res.sendFile('login.html', { root: path.join(__dirname, '/html') });
-                        
+    res.sendFile('login.html', { root: path.join(__dirname, '/public/html') });                   
+});
+
+app.get('/createacc', (req, res) => {        //get requests to the root ("/") will route here
+    res.sendFile('createaccount.html', { root: path.join(__dirname, '/public/html') });                    
+});
+
+app.get('/inventory', (req, res) => {        //get requests to the root ("/") will route here
+    res.sendFile('inventory.html', { root: path.join(__dirname, '/public/html') });                    
+});
+app.get('/additem', (req, res) => {        //get requests to the root ("/") will route here
+    res.sendFile('additem.html', { root: path.join(__dirname, '/public/html') });                    
+});
+app.get('/addnew', (req, res) => {        //get requests to the root ("/") will route here
+    res.sendFile('addnewitem.html', { root: path.join(__dirname, '/public/html') });                    
+});
+
+app.get('/addold', (req, res) => {        //get requests to the root ("/") will route here
+    res.sendFile('addolditem.html', { root: path.join(__dirname, '/public/html') });                    
+});
+
+app.get('/nutrition', (req, res) => {        //get requests to the root ("/") will route here
+    res.sendFile('nutrition.html', { root: path.join(__dirname, '/public/components') });                    
 });
 
 app.listen(port, () => {
