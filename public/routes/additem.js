6e5cb73805579e1655151dc662de2bc25b587892
emilyrn18 <emilyrn18@vt.edu>
2022-04-24 22:16:55 -0400

    Worked on addnewitem code

--- public/routes/additem.js
+++ public/routes/additem.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
+// const models = require('../models/foodtypeselector');
 
 //The first page of the website '/'
 router.get('/', function(req, res) {
     res.render('additem')
 })
 
-// router.post('/additem', models.additem)
+// router.post('/additem', models.addnewitem)
 module.exports = router
